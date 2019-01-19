b3169259f7dd46a56250bf752c2fe8843131c1ec
begimov <begimov@gmail.com>
2019-01-19 15:32:57 +0300

    UserSocial model class updated

--- src/Models/UserSocial.php
+++ src/Models/UserSocial.php
@@ -8,6 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class UserSocial extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users_social';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'social_id', 'service',
+    ];
+
+    /**
      * Define an inverse User relationship.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
