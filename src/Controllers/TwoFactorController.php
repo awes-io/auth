cab876ca020c4ceccdabde5ab7fc0c86d1ee34ad
begimov <begimov@gmail.com>
2019-01-20 16:10:52 +0300

    temporary inputs added to 2FA form in index template, countries passed in controller

--- src/Controllers/TwoFactorController.php
+++ src/Controllers/TwoFactorController.php
@@ -3,13 +3,16 @@
 namespace AwesIO\Auth\Controllers;
 
 use Illuminate\Http\Request;
+use AwesIO\Auth\Models\Country;
 use AwesIO\Auth\Controllers\Controller;
 
 class TwoFactorController extends Controller
 {
     public function index()
     {
-        return view('awesio-auth::twofactor.index');
+        $countries = Country::all();
+        
+        return view('awesio-auth::twofactor.index', compact('countries'));
     }
 
     public function store(Request $request)
--- views/twofactor/index.blade.php
+++ views/twofactor/index.blade.php
@@ -8,18 +8,36 @@
                 <div class="card-header">AWESIO Two Factor</div>
 
                 <div class="card-body">
-                    {{-- <form method="POST" action="{{ route('twofactor.store') }}">
+                    <form method="POST" action="{{ route('twofactor.store') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="dial_code" class="col-md-4 col-form-label text-md-right">{{ __('Country') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <select id="dial_code" class="form-control{{ $errors->has('dial_code') ? ' is-invalid' : '' }}" name="dial_code" value="{{ old('dial_code') }}">
+                                    @foreach ($countries as $country)
+                                        <option value="{{ $country->dial_code }}">{{ $country->name }} +{{ $country->dial_code }}</option>
+                                    @endforeach
+                                </select>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('dial_code'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('dial_code') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
+
+                                @if ($errors->has('phone'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone') }}</strong>
                                     </span>
                                 @endif
                             </div>
@@ -28,11 +46,11 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Save') }}
                                 </button>
                             </div>
                         </div>
-                    </form> --}}
+                    </form>
                 </div>
             </div>
 
