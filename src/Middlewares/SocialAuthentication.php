176ae47326ced906c2e8c48f17a5bb714d9054d1
begimov <begimov@gmail.com>
2019-01-20 10:01:30 +0300

    base SocialAuthentication middleware implemented

--- src/Middlewares/SocialAuthentication.php
+++ src/Middlewares/SocialAuthentication.php
@@ -15,7 +15,17 @@ class SocialAuthentication
      */
     public function handle($request, Closure $next)
     {
-        dd($request->service);
+        if (! $this->isServiceAvailable($request->service)) {
+            return redirect()->route('login');
+        }
         return $next($request);
+    }
+
+    protected function isServiceAvailable($service)
+    {
+        return in_array(
+            strtolower($service), 
+            array_keys(config('awesio-auth.socialite.services'))
+        );
     }
 }
