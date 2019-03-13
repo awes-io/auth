3b7d96958d81d1022aa5041c46dd1f736c5723a3
Yevhen L <yevhen.lisovenko@awescode.de>
2019-03-13 15:23:58 +0000

    Update verify.blade.php

--- views/auth/verify.blade.php
+++ views/auth/verify.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">AWESIO {{ __('Verify email by code') }}</div>
+                <div class="card-header">{{ __('Verify email by code') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('verification.code.verify') }}">
