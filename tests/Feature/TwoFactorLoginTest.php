ce279741e2c76cb2365685b4dea18584705e4ab1
begimov <begimov@gmail.com>
2019-05-23 16:43:44 +0300

    new tests implemented

--- tests/Feature/TwoFactorLoginTest.php
+++ tests/Feature/TwoFactorLoginTest.php
@@ -3,6 +3,7 @@
 namespace AwesIO\Auth\Tests\Feature;
 
 use AwesIO\Auth\Tests\TestCase;
+use AwesIO\Auth\Tests\Stubs\User;
 
 class TwoFactorLoginTest extends TestCase
 {
@@ -11,5 +12,18 @@ class TwoFactorLoginTest extends TestCase
     {
         $this->get('login/twofactor/verify')
             ->assertViewIs('awesio-auth::twofactor.verify');
+    }
+
+    /** @test */
+    public function it_verifies_two_factor_auth()
+    {
+        $user = factory(User::class)->create();
+
+        $this->withSession(['two_factor' => (object)[
+            'user_id' => $user->id,
+            'remember' => true
+        ]])->post('login/twofactor/verify', [
+            'token' => uniqid()
+        ])->assertStatus(302);
     }
 }
